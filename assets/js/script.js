bd380285343c8ed00619b65a4f3ed4b7e4f1c821
RhiH <rhi.herbert@gmail.com>
2022-04-21 20:15:59 +0000

    edit first stage js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+//wait for DOM to finish loading before running the game
+//get the button elemnets and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+            }
+        })
+    }
+}
+
 function runGame(){
 
 }
@@ -21,6 +40,4 @@ function displayAdditionQuestion(){
 function displaySubtractQuestion(){
 
 }
-function displayMultiplyQuestion(){
-    
-}
+function displayMultiplyQuestion(){}
