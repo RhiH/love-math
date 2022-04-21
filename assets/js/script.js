ae87e75de7f2a9213c47450b59b4b3d2137c2714
RhiH <rhi.herbert@gmail.com>
2022-04-21 20:00:25 +0000

    set up js game structure

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
