b5c6ac93e06c43a782dc8a244b088a3a97e99f43
RhiH <rhi.herbert@gmail.com>
2022-04-21 20:23:43 +0000

    js correcting initial code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
 //wait for DOM to finish loading before running the game
-//get the button elemnets and add event listeners to them
+//get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function(){
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-            else {
+            } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
-            }
-        })
+        });
     }
-}
+});
+
+
 
 function runGame(){
 
