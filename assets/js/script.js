2e275f5ec3bc2b9cac50765fbaa3ca4e0ce76718
RhiH <rhi.herbert@gmail.com>
2022-04-27 20:07:49 +0000

    amend text subtract error

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ function calculateCorrectAnswer(){
 
 }
 /**
- * Gets the current scor from the DOM and increments it by 1
+ * Gets the current score from the DOM and increments it by 1
  */
 function incrementScore(){
 
@@ -108,7 +108,7 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
