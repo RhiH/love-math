8d272dfab9a4cfb58074c72a7c223ebe79f6053b
RhiH <rhi.herbert@gmail.com>
2022-04-27 20:27:47 +0000

    edit to division calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,7 @@ function calculateCorrectAnswer(){
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     }else if (operator === "/") {
-        return [operand1 - operand2, "division"];
+        return [operand1 / operand2, "division"];
     } else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplementedoperator ${operator}.Aborting`;
