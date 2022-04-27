b276e11001bd3f7b221e16180d61b79e3ff11ded
RhiH <rhi.herbert@gmail.com>
2022-04-27 20:08:27 +0000

    amend multiply text error

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,7 +113,7 @@ function displaySubtractQuestion(operand1, operand2){
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
